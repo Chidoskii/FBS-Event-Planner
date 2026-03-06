349ab469cd1ec4da36131090924b844f6f49f973
Chidi Okpara <chididokpara@gmail.com>
2026-03-06 08:18:14 -0800

    Remove Tailwind and Untitled UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { RouteProvider } from "@/providers/route-provider";
-import { ThemeProvider } from "@/providers/theme-provider";
 import "./css/index.css";
-import "./css/theme.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
-      <RouteProvider>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </RouteProvider>
+      <App />
     </StrictMode>
   </BrowserRouter>,
 );
